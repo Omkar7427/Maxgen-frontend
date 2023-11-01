301005a6cbae4e314a2ea1367222978913a2fe42
Omkar Patil <omkarpt1996@gmail.com>
2023-11-01 12:15:34 +0530

    add all files of Frontend

---
+++ Js/heading.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById("omkar").innerHTML = "Omkar Patil";
+  }
+  function myFunction1() {
+    document.getElementById("amol").innerHTML = "Amol Salekar";
+  }
+  function myFunction2() {
+    document.getElementById("fawad").innerHTML = "Faead Ali";
+  }
